aa43eea32c710735bbc2791c789e263f57833929
piroyoung <piroyoung@users.noreply.github.com>
2023-05-02 17:14:09 +0900

    init

---
+++ cache.go
@@ -0,0 +1,37 @@
+package tsn
+
+import (
+	"context"
+	"github.com/anaregdesign/papaya/cache/graph"
+	"github.com/anaregdesign/papaya/nlp"
+	"time"
+)
+
+type TokenCache struct {
+	window int
+	dic    *nlp.Dictionary
+	c      *graph.GraphCache[int, string]
+}
+
+func NewTokenCache(ctx context.Context, window int) *TokenCache {
+	return &TokenCache{
+		window: window,
+		dic:    nlp.NewDictionary(),
+		c:      graph.NewGraphCache[int, string](ctx, 30*time.Minute),
+	}
+}
+
+func (c *TokenCache) Add(words []string) {
+	cbow := c.dic.Words2CBOW(words, c.window)
+	for _, w := range cbow {
+		c.c.AddVertex(w.Source, c.dic.ID2Word[w.Source])
+		for k, v := range w.Bow {
+			c.c.AddVertex(k, c.dic.ID2Word[k])
+			c.c.AddEdge(w.Source, k, float64(v))
+		}
+	}
+}
+
+func (c *TokenCache) NeighborTFiDFLog(word string, step int, top int) *graph.Graph[int, string] {
+	return c.c.NeighborTFiDFLog(c.dic.Word2ID[word], step, top)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/anaregdesign/tsn
 
 go 1.20
+
+require github.com/anaregdesign/papaya v0.0.2
+
+require golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53 // indirect
